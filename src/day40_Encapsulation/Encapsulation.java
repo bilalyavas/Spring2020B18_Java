cba30e70e066ebb7cabee16846fcbf049fd27198
cybertekschool <github@cybertekschool.com>
2020-05-15 10:33:22 -0400

    Day40 CLass

---
+++ src/day40_Encapsulation/Encapsulation.java
@@ -0,0 +1,17 @@
+package day40_Encapsulation;
+
+public class Encapsulation {
+
+    private long SSN;
+
+    // getter: read only
+   long getSSN(){
+        return SSN;
+    }
+
+    // setter: write only
+    void setSSN(long SSN){
+        this.SSN = SSN;
+    }
+
+}

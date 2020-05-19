6b64fc743bcd3c5f61522e3c20a78a13d1610de7
cybertekschool <github@cybertekschool.com>
2020-05-19 11:00:17 -0400

    Day42 CLass

---
+++ src/day42_Inheritance/Task01/Company.java
@@ -0,0 +1,16 @@
+package day42_Inheritance.Task01;
+/*
+create a class called company:
+                    create 3 objects of employee and set thier info
+                    create an array of employees and store all those employee objects
+                    use regular for loop to print out each employee' name and employeeID
+
+ */
+
+public class Company {
+
+
+
+
+
+}

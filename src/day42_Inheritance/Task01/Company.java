362b0132c694b7a621f078a528b433f24b3529ba
cybertekschool <github@cybertekschool.com>
2020-05-19 11:29:27 -0400

    Day42 CLass

--- src/day42_Inheritance/Task01/Company.java
+++ src/day42_Inheritance/Task01/Company.java
@@ -9,7 +9,33 @@ create a class called company:
 
 public class Company {
 
+    public static Employee employee1;
+    public static Employee employee2;
+    public static Employee employee3;
 
+    static{
+        employee1 = new Employee();
+        employee1.setEmployeeInfo("Messi", 32, 'M', 140_000, 13654,"SDET");
+
+        employee2 = new Employee();
+        employee2.setEmployeeInfo("Aigerim", 18, 'F', 130_000, 65654,"QA");
+
+        employee3 = new Employee();
+        employee3.setEmployeeInfo("Safar", 25, 'M', 128_000, 6599,"BA");
+
+    }
+
+    public static void main(String[] args) {
+
+        Employee[] employees = {employee1, employee2, employee3};
+
+        for(int i =0; i < employees.length; i++){
+           // System.out.println(employees[i]);
+            System.out.println("Name: "+ employees[i].name +", ID: "+ employees[i].employeeID );
+        }
+
+
+    }
 
 
 

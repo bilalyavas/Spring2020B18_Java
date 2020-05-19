b02e88eb4a4e3da72a2aa204acb90bf139b9b4e9
cybertekschool <github@cybertekschool.com>
2020-05-19 12:47:36 -0400

    Day42 CLass

---
+++ src/day42_Inheritance/Data.java
@@ -0,0 +1,10 @@
+package day42_Inheritance;
+
+public class Data {
+
+    public static String publicData = "public";
+    protected static String protectedData = "protected";
+    static String defaultData = "default";
+    private static String privateData = "private";
+
+}

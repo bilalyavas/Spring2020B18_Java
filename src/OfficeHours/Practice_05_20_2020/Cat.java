162a4ed1dd274c1314d3b7bb26d1b12d4d0e3b33
cybertekschool <github@cybertekschool.com>
2020-05-20 15:52:22 -0400

    Office Hour: 05/20/2020

--- src/OfficeHours/Practice_05_20_2020/Cat.java
+++ src/OfficeHours/Practice_05_20_2020/Cat.java
@@ -5,18 +5,22 @@ public class Cat extends Pet{
     /*
     Cat:
 		variables:	name, age, color, size, breed, isPet, numberOfeyes
-		methods: eat, sleep, drink, toString, scratch
+		methods: eat, sleep, drink, toString, scratch, setInfo
 
 		not inherited:
 		    scratch
 
      */
 
+    public Cat(String name, int age, String color, String size, String breed){
+       setInfo(name, age, color, size, breed);
+    }
+
 
 
     public void scratch(){
+
         System.out.println(name+" is scratching");
     }
-
 
 }
--- src/OfficeHours/Practice_05_20_2020/House.java
+++ src/OfficeHours/Practice_05_20_2020/House.java
@@ -4,9 +4,23 @@ public class House {
 
     public static void main(String[] args) {
 
-        Dog dog1 = new Dog();
-            dog1.setInfo("Karabas",5,"black","large","Husky");
+        Dog dog1 = new Dog("Karabas",5,"black","large","Husky");
+         //   dog1.setInfo("Karabas",5,"black","large","Husky");
+
+        Dog dog2 = new Dog("Rindik", 5, "White", "Huge","Kangal");
+
         System.out.println(dog1);
+        System.out.println(dog2);
+
+        Cat cat1 = new Cat("Muffin",7,"white","medium","Ragdoll");
+          //  cat1.setInfo("Muffin",7,"white","medium","Ragdoll");
+
+        System.out.println(cat1);
+
+
+
+
+
 
 
 

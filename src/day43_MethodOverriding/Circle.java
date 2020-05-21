f82e3a1c604dbc916cea80d6942d324fc02ce6b3
cybertekschool <github@cybertekschool.com>
2020-05-21 10:22:48 -0400

    Day43 class

---
+++ src/day43_MethodOverriding/Circle.java
@@ -0,0 +1,13 @@
+package day43_MethodOverriding;
+
+public class Circle extends Shape{
+
+    public double radius;
+    public static double PI = 3.14;
+
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+
+}

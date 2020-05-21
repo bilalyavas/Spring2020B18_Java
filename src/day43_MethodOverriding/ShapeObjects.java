9433c6b80f27d8405fdf1905a273994c3eef9652
cybertekschool <github@cybertekschool.com>
2020-05-21 10:46:31 -0400

    Day43 class

--- src/day43_MethodOverriding/ShapeObjects.java
+++ src/day43_MethodOverriding/ShapeObjects.java
@@ -9,7 +9,7 @@ public class ShapeObjects {
         System.out.println(circle1.radius);
 
 
-        circle1.calculateArea();  // bug
+        circle1.calculateArea();  // bug  suppose to be: Pi * r * r
         circle1.calculatePerimeter(); // bug
 
         System.out.println("=========================================");
@@ -19,10 +19,18 @@ public class ShapeObjects {
         System.out.println(rec1.length);
         System.out.println(rec1.width);
 
-        rec1.calculateArea();   // bug
+        rec1.calculateArea();   // bug suppose to be: w *l
         rec1.calculatePerimeter(); // bug
 
         System.out.println("=========================================");
+
+        Square sq1 = new Square(4);
+
+        System.out.println(sq1.side);
+
+        sq1.calculateArea();  // bug  suppose to be: side * side
+        sq1.calculatePerimeter(); // bug
+
 
     }
 

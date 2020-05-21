e8a7124a0ba6add273fd5c56ed14b46d71e2ef3e
cybertekschool <github@cybertekschool.com>
2020-05-21 10:14:33 -0400

    Day43 class

---
+++ src/day43_MethodOverriding/Shape.java
@@ -0,0 +1,17 @@
+package day43_MethodOverriding;
+/*
+	1. create a class called Shape
+						vairables: area, perimeter
+						methods: calculateArea(), calculatePerimeter()
+ */
+public class Shape {
+
+    public void calculateArea(){
+        System.out.println("Area of the shape: "+0);
+    }
+
+    public void calculatePerimeter(){
+        System.out.println("Perimeter of the shape: "+0);
+    }
+
+}

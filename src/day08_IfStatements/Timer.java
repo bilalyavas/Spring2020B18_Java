046c52cc344d0408a2e0b734c54e23cc2d6145d4
cybertekschool <github@cybertekschool.com>
2020-05-28 16:02:29 -0400

    Day44 Class

--- src/day08_IfStatements/Timer.java
+++ src/day08_IfStatements/Timer.java
@@ -14,7 +14,7 @@ public class Timer {
                 break;
             for (int z=59; z >0 ; --z) {
                 System.out.println((i-1)+" minutes and "+z+" seconds left");
-                    Thread.sleep(1000);
+                   Thread.sleep(1000);
             }
         }
 
--- src/day44_Exceptions/Practice2.java
+++ src/day44_Exceptions/Practice2.java
@@ -3,9 +3,12 @@ package day44_Exceptions;
 public class Practice2 {
 
     public static void main(String[] args) {
-        System.out.println("Java");
-        waits(3.5);
+
+        System.out.println("Hello");
+        waits(5);
         System.out.println("Cybertek");
+
+
 
     }
 
@@ -13,7 +16,10 @@ public class Practice2 {
     public static void waits(double seconds){
         try {
             Thread.sleep((long)(seconds * 1000) );
-        }catch (Exception e){ }
+
+        }catch (Exception e){
+
+        }
 
     }
 

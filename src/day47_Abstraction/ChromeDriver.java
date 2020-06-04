a461f353707fb94c6bb4d51a55f90d1c5a771d9e
cybertekschool <github@cybertekschool.com>
2020-06-04 11:00:34 -0400

    Day47 Class

---
+++ src/day47_Abstraction/ChromeDriver.java
@@ -0,0 +1,10 @@
+package day47_Abstraction;
+
+public class ChromeDriver extends RemoteWebDriver{
+
+    @Override
+    public void get(String URL){
+        System.out.println("Opening "+URL+" in chrome browser");
+    }
+
+}
---
+++ src/day47_Abstraction/driverObjects.java
@@ -0,0 +1,13 @@
+package day47_Abstraction;
+
+public class driverObjects {
+
+    public static void main(String[] args) {
+
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+
+
+    }
+
+}

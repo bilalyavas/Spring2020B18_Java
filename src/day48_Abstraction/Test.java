c3930cdc54a5a37da5017c0afff522c7b5547678
cybertekschool <github@cybertekschool.com>
2020-06-08 12:00:32 -0400

    Day48 Class

---
+++ src/day48_Abstraction/Test.java
@@ -0,0 +1,19 @@
+package day48_Abstraction;
+
+public class Test implements Interface_Intro {
+
+    @Override
+    public void method1(){
+
+    }
+
+    public void method2(){
+
+    }
+
+    public void method3(){
+
+    }
+
+
+}

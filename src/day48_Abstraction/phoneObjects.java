b8a1bbd58e4e52a250aef662ddd954ef2a67da85
cybertekschool <github@cybertekschool.com>
2020-06-08 11:29:35 -0400

    Day48 Class

--- src/day48_Abstraction/phoneObjects.java
+++ src/day48_Abstraction/phoneObjects.java
@@ -13,10 +13,19 @@ public class phoneObjects {
         iphone1.calling(911);
         iphone1.texting(987456);
 
+        // iphone1.freezing();
+
         System.out.println("===============================");
 
+        Samsung samsung1 = new Samsung("Note10 Plus", 1200, "Large");
 
+        System.out.println(samsung1);
 
+        samsung1.calling(123456);
+       // samsung1.faceTiming(123456);
+        samsung1.freezing();
+
+        samsung1.texting(123456);
 
 
     }

932414d3252bd1a31d466972abe16913bd32aefa
cybertekschool <github@cybertekschool.com>
2020-06-15 16:00:46 -0400

    Day50 Class

---
+++ src/day50_Polymorphism/Abstraction.java
@@ -0,0 +1,19 @@
+package day50_Polymorphism;
+
+interface I{
+    void method2(); // public abstract
+    abstract int method3();
+}
+
+abstract class A{
+
+    public abstract void method1();
+
+}
+
+public class Abstraction {
+
+   // public abstract void method1();
+
+
+}

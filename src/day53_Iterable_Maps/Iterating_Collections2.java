bc4006f6d15f6e448ce60449b96c95b47cf39bf8
cybertekschool <github@cybertekschool.com>
2020-06-25 12:38:46 -0400

    Day53 Class

--- src/day53_Iterable_Maps/Iterating_Collections2.java
+++ src/day53_Iterable_Maps/Iterating_Collections2.java
@@ -23,6 +23,49 @@ public class Iterating_Collections2 {
         System.out.println(names);
 
         System.out.println("==============================================");
+        LinkedHashSet<String> students = new LinkedHashSet<>();
+        students.addAll(Arrays.asList( "Mehmet", "Mohammed", "Yucel", "Sha",
+                "Ozgur", "Ahmet", "Osmanj", "Ozgur", "Ozgur", "Ozgur", "Ozgur" , "Irina"  ) );
+
+        for( Iterator<String> I = students.iterator(); I.hasNext() ;  ){
+            String s = I.next();
+            if(s.contains("m") || s.contains("M")){
+                I.remove();
+            }
+        }
+
+        System.out.println(students);
+
+        System.out.println("=====================================================");
+        LinkedHashSet<String> n = new LinkedHashSet<>();
+        n.addAll(Arrays.asList( "Mehmet", "Mohammed", "Yucel", "Sha",
+                "Ozgur", "Ahmet", "Osmanj", "Ozgur", "Ozgur", "Ozgur", "Ozgur" , "Irina"  ) );
+
+        n.removeIf( s -> s.contains("m") || s.contains("M") );
+
+        System.out.println(n);
+
+        System.out.println("==========================================");
+
+        LinkedHashSet<String> T = new LinkedHashSet<>();
+        T.addAll(Arrays.asList( "Mehmet", "Mohammed", "Yucel", "Sha",
+                "Ozgur", "Ahmet", "Osman", "Ozgur", "Ozgur", "Ozgur", "Ozgur" , "Irina"  ) );
+
+        T.removeAll( Arrays.asList( "Mehmet", "Ozgur", "Mohammed" ));
+
+        System.out.println(T);
+
+        System.out.println("=================================================");
+        LinkedHashSet<String> R = new LinkedHashSet<>();
+        R.addAll(Arrays.asList( "Mehmet", "Mohammed", "Yucel", "Sha",
+                "Ozgur", "Ahmet", "Osman", "Ozgur", "Ozgur", "Ozgur", "Ozgur" , "Irina"  ) );
+
+        R.retainAll( Arrays.asList( "Yucel", "Sha", "Ahmet" ) );
+
+        System.out.println(R);
+
+
+
 
     }
 

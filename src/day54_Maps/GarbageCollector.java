eb5df20ffd5a487767b586dc0b347d95c4bf7fc2
cybertekschool <github@cybertekschool.com>
2020-06-29 14:29:09 -0400

    Day54 Class

---
+++ src/day54_Maps/GarbageCollector.java
@@ -0,0 +1,33 @@
+package day54_Maps;
+
+public class GarbageCollector {
+
+    public static void main(String[] args) {
+        GarbageCollector obj =  new GarbageCollector(); // will not be collected by garbage collector
+                            new GarbageCollector(); // will be collected by garbage collector
+
+
+        System.out.println("Done");
+
+        // unreference an object:
+
+        String str = new String("Cybertek");
+               str = new String("MIT");
+
+        System.out.println(str);
+
+
+        Integer num = new Integer(123);
+                num =  new Integer(124);
+
+                    //123 will be collected by garbage collector
+
+        Double n = new Double(0.5);
+               n = null;
+
+        System.out.println(n);
+
+
+
+    }
+}
